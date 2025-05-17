b0353d233dbc987f1f8182cc54766b3ec74759eb
md.munna <mk8761174@gmail.com>
2025-05-17 19:17:49 +0600

    Creating a Table with Multiple Columns and Different Data Types and Create, Update and Delete Database and Data Types in POSTGRES.mp4

---
+++ query.sql
@@ -0,0 +1,22 @@
+-- Active: 1747415487712@@localhost@5432@ph
+
+SELECT * FROM person2;
+
+ALTER TABLE person2
+ADD COLUMN email VARCHAR(30) NOT NULL DEFAULT 'default@gmail.com';
+INSERT INTO person2 VALUES(9,'mojnu',48,'mojnu@gamil.com')
+
+ALTER TABLE person2
+DROP COLUMN email
+
+
+
+alter Table person2
+RENAME COLUMN age to user_age;
+alter Table person2
+alter COLUMN user_name type VARCHAR(50)
+
+alter Table person2
+alter COLUMN user_age set not NULL;
+alter Table person2
+alter COLUMN user_age DROP not NULL;
